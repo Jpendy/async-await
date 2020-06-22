76bfeed49515911fde1b970ac2a8d5f90dcf23a9
Jpendy <Jpendy256@gmail.com>
2020-06-22 15:47:07 -0700

    made seed function

---
+++ __tests__/seed.test.js
@@ -0,0 +1,25 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+const { seed } = require('../lib/data-helpers/seed');
+
+
+describe('seed functions', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+
+
+});
